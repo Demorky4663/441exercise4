3f1198b3161b49b4f3e33d1da356760ece17156b
Demorky4663 <103400343+Demorky4663@users.noreply.github.com>
2022-04-19 15:09:45 +0800

    Add files via upload

---
+++ jia.js
@@ -0,0 +1,11 @@
+/*RonghuaWu Demorky
+1.Easy to operate HTML
+2.Relatively safe basic language
+3.JavaScript supports distributed computing
+*/
+function jia(){
+    var a=document.getElementById('a').value;
+    var b=document.getElementById('b').value;
+    var c=document.getElementById('c');
+    c.value=Number(a)+Number(b);
+}
